e15c19d1bfe8b8318467c37a7d1157e9059de2fa
Aviad Hahami <aviadhahami@gmail.com>
2016-09-02 01:13:23 +0300

    changed to url

--- server/clientSettings/clientSettings.js
+++ server/clientSettings/clientSettings.js
@@ -12,7 +12,7 @@ module.exports = {
 				{
 					"type":"web_url",
 					"title":"Search Settings",
-					"payload":"client/index.html"
+					"url":"client/index.html"
 				},
 				{
 					"type":"postback",
