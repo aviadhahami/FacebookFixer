632c46e4e3a71e17e3dc57e8b390a5f176855bb9
Aviad Hahami <aviadhahami@gmail.com>
2016-09-01 23:17:04 +0300

    YES YES YES POINTER PARTY!

---
+++ server/actions/technician.actions.js
@@ -0,0 +1,16 @@
+/**
+ * Created by aviad on 9/1/2016.
+ */
+function fetchRemote(params) {
+	
+}
+
+
+function fetchNearby(params){
+	
+};
+
+module.exports = {
+	fetchRemote:fetchRemote,
+	fetchNearby: fetchNearby
+}
