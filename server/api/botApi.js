4aecb5e0b6ab2b80cb3dc391346207b51a547567
Aviad Hahami <aviadhahami@gmail.com>
2016-09-01 15:19:49 +0300

    refactor

--- server/api/botApi.js
+++ server/api/botApi.js
@@ -8,24 +8,10 @@ module.exports = {
 			url: 'https://graph.facebook.com/v2.6/me/messages',
 			method: 'POST',
 			qs: {access_token: token},
-			recipient: {id: id},
-			sender_action: "typing_on"
+			json: {
+				recipient: {id: id},
+				sender_action: "typing_on"
+			}
 		}
 	}
 };
-// 	{
-// 		"recipient":{
-// 		"id":"USER_ID"
-// 	},
-//
-// 	}' "https://graph.facebook.com/v2.6/me/messages?access_token=PAGE_ACCESS_TOKEN
-// }
-// };
-// return {
-// 	url: 'https://graph.facebook.com/v2.6/me/messages',
-// 		qs: {access_token:token},
-// 	method: 'POST',
-// 		json: {
-// 	recipient: {id:sender},
-// 	message: messageData,
-// }
