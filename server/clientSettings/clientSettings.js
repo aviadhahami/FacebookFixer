865d3c4ceaa459da2e6ada144e532a0a12791936
Aviad Hahami <aviadhahami@gmail.com>
2016-09-02 01:15:33 +0300

    changed to url

--- server/clientSettings/clientSettings.js
+++ server/clientSettings/clientSettings.js
@@ -4,34 +4,34 @@
 
 
 module.exports = {
-	generateMenu:function(){
-		return{
-			"setting_type" : "call_to_actions",
-			"thread_state" : "existing_thread",
-			"call_to_actions":[
+	generateMenu: function () {
+		return {
+			"setting_type": "call_to_actions",
+			"thread_state": "existing_thread",
+			"call_to_actions": [
 				{
-					"type":"web_url",
-					"title":"Search Settings",
-					"url":"client/index.html"
+					"type": "web_url",
+					"title": "Search Settings",
+					"url": "/client/index.html"
 				},
 				{
-					"type":"postback",
-					"title":"History",
-					"payload":"History_Payload"
+					"type": "postback",
+					"title": "History",
+					"payload": "History_Payload"
 				},
 				{
-					"type":"postback",
-					"title":"Report A Bug",
-					"payload":"Report_A_Bug_Payload"
+					"type": "postback",
+					"title": "Report A Bug",
+					"payload": "Report_A_Bug_Payload"
 				}
 			]
 		}
 	},
-	generateGreeting:function(){
+	generateGreeting: function () {
 		return {
-			"setting_type":"greeting",
-			"greeting":{
-				"text":`Welcome to Fixer!
+			"setting_type": "greeting",
+			"greeting": {
+				"text": `Welcome to Fixer!
 				It is recommended to check the available settings`
 			}
 		}
