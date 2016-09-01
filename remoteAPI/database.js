a95970d86251353c73718ef0d7e21c5adbc34c23
Aviad Hahami <aviadhahami@gmail.com>
2016-09-01 18:58:14 +0300

    fix to db

--- remoteAPI/database.js
+++ remoteAPI/database.js
@@ -5,202 +5,202 @@
 
 'use strict';
 
-{
-    "results": [
-        {
-            "name": "Pizza Hut",
-            "website": "http://pizzahut.co.il",
-            "location": {
-                "country": "Israel",
-                "city": "Tel-Aviv",
-                "adress": "Yigal Alon 51"
-            },
-            "rating": "3.3",
-            "opening hours": {
-                "sunday": "11:00-00:00",
-                "monday": "11:00-00:00",
-                "tuesday": "11:00-00:00",
-                "wednesday": "11:00-00:00",
-                "thursday": "11:00-00:00",
-                "friday": "11:00-00:00",
-                "saturday": "11:00-00:00"
-            },
-            "phone number": "1700506070",
-            "service": {
-                "type": "restaurant",
-                "sub-type": "italian"
-            },
-            "images": [
-                {
-                    "img1": "http://img.d.co.il/homepages/UploadImages/80111456/logo_b8ef6244-d484-447f-8692-a08484c0315b.jpg"
-                },
-                {
-                    "img2": "http://www.mishlohim.co.il/img/logos/Second_Business_pic3789.jpg"
-                },
-                {
-                    "img3": "http://blog.tapuz.co.il/kzataheret/images/1860570_988.jpg"
-                }
-            ]
-        },
-        {
-            "name": "Philippe",
-            "website": "http://philippe.rest.co.il/",
-            "location": {
-                "country": "Israel",
-                "city": "Tel-Aviv",
-                "adress": "Karlibach 20"
-            },
-            "rating": "4",
-            "opening hours": {
-                "sunday": "17:00-01:00",
-                "monday": "17:00-01:00",
-                "tuesday": "17:00-01:00",
-                "wednesday": "17:00-01:00",
-                "thursday": "17:00-01:00",
-                "friday": "12:00-02:00",
-                "saturday": "12:00-01:00"
-            },
-            "phone number": "0539443290",
-            "service": {
-                "type": "restaurant",
-                "sub-type": "italian"
-            },
-            "images": [
-                {
-                    "img1": "http://images.rest.co.il/Customers/80056880/20b46b8448df4a50b4be40926694dc27.jpg"
-                },
-                {
-                    "img2": "http://images.rest.co.il/Customers/80056880/0764615d8ccb481a971ed2a824db22e7.jpg"
-                },
-                {
-                    "img3": "http://images.rest.co.il/Customers/80056880/471b7ba6d5a24b629c1f450e521dd202.jpg"
-                }
-            ]
-        },
-        {
-            "name": "Pizza Seret",
-            "website": "",
-            "location": {
-                "country": "Israel",
-                "city": "Tel-Aviv",
-                "adress": "Ibn Gabirol 24"
-            },
-            "rating": "1",
-            "opening hours": {
-                "sunday": "09:00-03:00",
-                "monday": "09:00-03:00",
-                "tuesday": "09:00-03:00",
-                "wednesday": "09:00-03:00",
-                "thursday": "09:00-03:00",
-                "friday": "09:00-03:00",
-                "saturday": "09:00-03:00"
-            },
-            "phone number": "0539366008",
-            "service": {
-                "type": "restaurant",
-                "sub-type": "italian"
-            },
-            "images": [
-                {
-                    "img1": "http://4.bp.blogspot.com/-j4sAv-XcEtk/TzkLFI4cFqI/AAAAAAAAAp0/9tQKfHMFvXc/s1600/%25D7%25A4%25D7%2599%25D7%25A6%25D7%2594+%25D7%25A1%25D7%25A8%25D7%2598+%25D7%2594%25D7%25A4%25D7%2599%25D7%25A6%25D7%2594+%25D7%2594%25D7%2598%25D7%2595%25D7%2591%25D7%2594+%25D7%2591%25D7%25A2%25D7%2599%25D7%25A8.jpg"
-                }
-            ]
-        },
-        {
-            "name": "Gabi Mazganim",
-            "website": "www.gabiMaz.com",
-            "location": {
-                "country": "Israel",
-                "city": "Tel-Aviv",
-                "adress": "Shoham 11"
-            },
-            "rating": "5",
-            "opening hours": {
-                "sunday": "09:00-18:00",
-                "monday": "09:00-18:00",
-                "tuesday": "09:00-18:00",
-                "wednesday": "09:00-18:00",
-                "thursday": "09:00-18:00",
-                "friday": "09:00-12:00",
-                "saturday": "-"
-            },
-            "phone number": "0525366008",
-            "service": {
-                "type": "technician",
-                "sub-type": "airconditioning"
-            },
-            "images": [
-                {
-                    "img1": "http://www.stromva.com/wp-content/uploads/2016/08/air-mech-engineering-pty-ltd-kingsgrove-2208-thumbnail.jpg"
-                },
-                {
-                    "img2": "http://www.airxperts.com.au/wp-content/uploads/air-Conditioning-Sydney.jpg"
-                }
-            ]
-        },
-        {
-            "name": "Gabi Airconditioning",
-            "website": "www.gabiAir.co.il",
-            "location": {
-                "country": "Israel",
-                "city": "Tel-Aviv",
-                "adress": "Hasharon 15"
-            },
-            "rating": "3.7",
-            "opening hours": {
-                "sunday": "09:00-18:00",
-                "monday": "09:00-18:00",
-                "tuesday": "09:00-18:00",
-                "wednesday": "09:00-18:00",
-                "thursday": "09:00-18:00",
-                "friday": "09:00-12:00",
-                "saturday": "-"
-            },
-            "phone number": "0544678890",
-            "service": {
-                "type": "technician",
-                "sub-type": "airconditioning"
-            },
-            "images": [
-                {
-                    "img1": "http://www.stromva.com/wp-content/uploads/2016/08/air-mech-engineering-pty-ltd-kingsgrove-2208-thumbnail.jpg"
-                },
-                {
-                    "img2": "http://www.airxperts.com.au/wp-content/uploads/air-Conditioning-Sydney.jpg"
-                }
-            ]
-        },
-        {
-            "name": "GabriAir",
-            "website": "www.gabriAir.co.il",
-            "location": {
-                "country": "Israel",
-                "city": "Tel-Aviv",
-                "adress": "Ibn Gabirol 15"
-            },
-            "rating": "4.7",
-            "opening hours": {
-                "sunday": "09:00-18:00",
-                "monday": "09:00-18:00",
-                "tuesday": "09:00-18:00",
-                "wednesday": "09:00-18:00",
-                "thursday": "09:00-18:00",
-                "friday": "09:00-12:00",
-                "saturday": "-"
-            },
-            "phone number": "0548678890",
-            "service": {
-                "type": "technician",
-                "sub-type": "airconditioning"
-            },
-            "images": [
-                {
-                    "img1": "http://www.stromva.com/wp-content/uploads/2016/08/air-mech-engineering-pty-ltd-kingsgrove-2208-thumbnail.jpg"
-                },
-                {
-                    "img2": "http://www.airxperts.com.au/wp-content/uploads/air-Conditioning-Sydney.jpg"
-                }
-            ]
-        }
-    ]
+module.exports={
+	"results": [
+		{
+			"name": "Pizza Hut",
+			"website": "http://pizzahut.co.il",
+			"location": {
+				"country": "Israel",
+				"city": "Tel-Aviv",
+				"adress": "Yigal Alon 51"
+			},
+			"rating": "3.3",
+			"opening hours": {
+				"sunday": "11:00-00:00",
+				"monday": "11:00-00:00",
+				"tuesday": "11:00-00:00",
+				"wednesday": "11:00-00:00",
+				"thursday": "11:00-00:00",
+				"friday": "11:00-00:00",
+				"saturday": "11:00-00:00"
+			},
+			"phone number": "1700506070",
+			"service": {
+				"type": "restaurant",
+				"sub-type": "italian"
+			},
+			"images": [
+				{
+					"img1": "http://img.d.co.il/homepages/UploadImages/80111456/logo_b8ef6244-d484-447f-8692-a08484c0315b.jpg"
+				},
+				{
+					"img2": "http://www.mishlohim.co.il/img/logos/Second_Business_pic3789.jpg"
+				},
+				{
+					"img3": "http://blog.tapuz.co.il/kzataheret/images/1860570_988.jpg"
+				}
+			]
+		},
+		{
+			"name": "Philippe",
+			"website": "http://philippe.rest.co.il/",
+			"location": {
+				"country": "Israel",
+				"city": "Tel-Aviv",
+				"adress": "Karlibach 20"
+			},
+			"rating": "4",
+			"opening hours": {
+				"sunday": "17:00-01:00",
+				"monday": "17:00-01:00",
+				"tuesday": "17:00-01:00",
+				"wednesday": "17:00-01:00",
+				"thursday": "17:00-01:00",
+				"friday": "12:00-02:00",
+				"saturday": "12:00-01:00"
+			},
+			"phone number": "0539443290",
+			"service": {
+				"type": "restaurant",
+				"sub-type": "italian"
+			},
+			"images": [
+				{
+					"img1": "http://images.rest.co.il/Customers/80056880/20b46b8448df4a50b4be40926694dc27.jpg"
+				},
+				{
+					"img2": "http://images.rest.co.il/Customers/80056880/0764615d8ccb481a971ed2a824db22e7.jpg"
+				},
+				{
+					"img3": "http://images.rest.co.il/Customers/80056880/471b7ba6d5a24b629c1f450e521dd202.jpg"
+				}
+			]
+		},
+		{
+			"name": "Pizza Seret",
+			"website": "",
+			"location": {
+				"country": "Israel",
+				"city": "Tel-Aviv",
+				"adress": "Ibn Gabirol 24"
+			},
+			"rating": "1",
+			"opening hours": {
+				"sunday": "09:00-03:00",
+				"monday": "09:00-03:00",
+				"tuesday": "09:00-03:00",
+				"wednesday": "09:00-03:00",
+				"thursday": "09:00-03:00",
+				"friday": "09:00-03:00",
+				"saturday": "09:00-03:00"
+			},
+			"phone number": "0539366008",
+			"service": {
+				"type": "restaurant",
+				"sub-type": "italian"
+			},
+			"images": [
+				{
+					"img1": "http://4.bp.blogspot.com/-j4sAv-XcEtk/TzkLFI4cFqI/AAAAAAAAAp0/9tQKfHMFvXc/s1600/%25D7%25A4%25D7%2599%25D7%25A6%25D7%2594+%25D7%25A1%25D7%25A8%25D7%2598+%25D7%2594%25D7%25A4%25D7%2599%25D7%25A6%25D7%2594+%25D7%2594%25D7%2598%25D7%2595%25D7%2591%25D7%2594+%25D7%2591%25D7%25A2%25D7%2599%25D7%25A8.jpg"
+				}
+			]
+		},
+		{
+			"name": "Gabi Mazganim",
+			"website": "www.gabiMaz.com",
+			"location": {
+				"country": "Israel",
+				"city": "Tel-Aviv",
+				"adress": "Shoham 11"
+			},
+			"rating": "5",
+			"opening hours": {
+				"sunday": "09:00-18:00",
+				"monday": "09:00-18:00",
+				"tuesday": "09:00-18:00",
+				"wednesday": "09:00-18:00",
+				"thursday": "09:00-18:00",
+				"friday": "09:00-12:00",
+				"saturday": "-"
+			},
+			"phone number": "0525366008",
+			"service": {
+				"type": "technician",
+				"sub-type": "airconditioning"
+			},
+			"images": [
+				{
+					"img1": "http://www.stromva.com/wp-content/uploads/2016/08/air-mech-engineering-pty-ltd-kingsgrove-2208-thumbnail.jpg"
+				},
+				{
+					"img2": "http://www.airxperts.com.au/wp-content/uploads/air-Conditioning-Sydney.jpg"
+				}
+			]
+		},
+		{
+			"name": "Gabi Airconditioning",
+			"website": "www.gabiAir.co.il",
+			"location": {
+				"country": "Israel",
+				"city": "Tel-Aviv",
+				"adress": "Hasharon 15"
+			},
+			"rating": "3.7",
+			"opening hours": {
+				"sunday": "09:00-18:00",
+				"monday": "09:00-18:00",
+				"tuesday": "09:00-18:00",
+				"wednesday": "09:00-18:00",
+				"thursday": "09:00-18:00",
+				"friday": "09:00-12:00",
+				"saturday": "-"
+			},
+			"phone number": "0544678890",
+			"service": {
+				"type": "technician",
+				"sub-type": "airconditioning"
+			},
+			"images": [
+				{
+					"img1": "http://www.stromva.com/wp-content/uploads/2016/08/air-mech-engineering-pty-ltd-kingsgrove-2208-thumbnail.jpg"
+				},
+				{
+					"img2": "http://www.airxperts.com.au/wp-content/uploads/air-Conditioning-Sydney.jpg"
+				}
+			]
+		},
+		{
+			"name": "GabriAir",
+			"website": "www.gabriAir.co.il",
+			"location": {
+				"country": "Israel",
+				"city": "Tel-Aviv",
+				"adress": "Ibn Gabirol 15"
+			},
+			"rating": "4.7",
+			"opening hours": {
+				"sunday": "09:00-18:00",
+				"monday": "09:00-18:00",
+				"tuesday": "09:00-18:00",
+				"wednesday": "09:00-18:00",
+				"thursday": "09:00-18:00",
+				"friday": "09:00-12:00",
+				"saturday": "-"
+			},
+			"phone number": "0548678890",
+			"service": {
+				"type": "technician",
+				"sub-type": "airconditioning"
+			},
+			"images": [
+				{
+					"img1": "http://www.stromva.com/wp-content/uploads/2016/08/air-mech-engineering-pty-ltd-kingsgrove-2208-thumbnail.jpg"
+				},
+				{
+					"img2": "http://www.airxperts.com.au/wp-content/uploads/air-Conditioning-Sydney.jpg"
+				}
+			]
+		}
+	]
 }
