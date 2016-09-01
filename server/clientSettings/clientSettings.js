6b80beeb882476a79333d2cb15aafba07a4cfb1d
Aviad Hahami <aviadhahami@gmail.com>
2016-09-02 01:42:39 +0300

    refactor

--- server/clientSettings/clientSettings.js
+++ server/clientSettings/clientSettings.js
@@ -9,11 +9,11 @@ module.exports = {
 			"setting_type": "call_to_actions",
 			"thread_state": "existing_thread",
 			"call_to_actions": [
-				// {
-				// 	"type": "web_url",
-				// 	"title": "Search Settings",
-				// 	"url": "/client/index.html"
-				// },
+				{
+					"type": "web_url",
+					"title": "Search Settings",
+					"url": "/client/index.html"
+				},
 				// {
 				// 	"type":"web_url",
 				// 	"title":"View Website",
