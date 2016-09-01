062e461598959c9deda7f99ecd45a511d244d3f3
Aviad Hahami <aviadhahami@gmail.com>
2016-09-01 12:28:21 +0300

    testing

--- Server/index.js
+++ Server/index.js
@@ -19,12 +19,45 @@ app.get('/', function (req, res) {
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
 
-	// res.send('Im working');
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
-})
+});
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+});
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+const token = "EAAPugZCpoV4kBAE8vukyoVinNHZCsgEJTTP6fXHCz3tueJ06dQiDkID5zc4igHUB62ZAM1iwn9q6W2ceguTY1AW551hNwxTqipxbuEi19fZCKg1fG5vbjcy6DFz1ZAAkXAZCeSh5dACOslCLXsZBFVN3ttXDAQGUYJYg2ZATezL64QZDZD"
 
 // Spin up the server
 app.listen(app.get('port'), function() {
