c1c422da85199bb9a909a4ca9b10583f62c00db2
Aviad Hahami <aviadhahami@gmail.com>
2016-09-01 19:11:56 +0300

    adding sms api

--- server/api/smsAPI.js
+++ server/api/smsAPI.js
@@ -7,11 +7,15 @@ const twillio_tokens = require('./../secrets').twilio;
 const twilio = require('twilio');
 
 // Find your account sid and auth token in your Twilio account Console.
-const client = twilio('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN');
+const client = twilio(twillio_tokens.SID, twillio_tokens.token);
 
-// Send the text message.
-client.sendMessage({
-	to: 'YOUR_NUMBER',
-	from: 'YOUR_TWILIO_NUMBER',
-	body: 'Hello from Twilio!'
-});
+module.exports={
+	sendText:function (number,msg) {
+		// Send the text message.
+		client.sendMessage({
+			to: number,
+			from: twillio_tokens.self_number,
+			body: msg
+		});
+	}
+};
