30d4333526280a96ce550e374ff7499fbce14a61
Aviad Hahami <aviadhahami@gmail.com>
2016-09-02 01:41:07 +0300

    refactor

--- server/clientSettings/clientSettings.js
+++ server/clientSettings/clientSettings.js
@@ -9,16 +9,16 @@ module.exports = {
 			"setting_type": "call_to_actions",
 			"thread_state": "existing_thread",
 			"call_to_actions": [
-				{
-					"type": "web_url",
-					"title": "Search Settings",
-					"url": "/client/index.html"
-				},
-				{
-					"type":"web_url",
-					"title":"View Website",
-					"url":"http://google.com/"
-				},
+				// {
+				// 	"type": "web_url",
+				// 	"title": "Search Settings",
+				// 	"url": "/client/index.html"
+				// },
+				// {
+				// 	"type":"web_url",
+				// 	"title":"View Website",
+				// 	"url":"http://google.com/"
+				// },
 				{
 					"type": "postback",
 					"title": "History",
