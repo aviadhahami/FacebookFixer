b71abe71a5c9c910e5c27312f9c72c59796ba22d
Aviad Hahami <aviadhahami@gmail.com>
2016-09-02 03:49:17 +0300

    WHAT THE PROMISE FUCK

--- server/actions/restaurant.actions.js
+++ server/actions/restaurant.actions.js
@@ -1,5 +1,20 @@
+const googleAPI = require('./../api/googlePlacesAPI');
+const q = require('q');
+
 function fetchRemote(params) {
-	
+	let deferred = q.defer();
+	let location = params.location;
+	googleAPI.decodeLocation(location).then(function(parsedLocation){
+		googleAPI.searchByQuery(`${params.type} restaurant`,parsedLocation,1000).then(function(res){
+			deferred.resolve(res);
+		}, function(err){
+			deferred.reject(err);
+		})
+	},function(err){
+		// Don't care
+		deferred.reject(err);
+	});
+	return deferred.promise;
 }
 
 
@@ -8,6 +23,6 @@ function fetchNearby(params){
 }
 
 module.exports = {
-	fetchRemote:fetchRemote,
-	fetchNearby: fetchNearby
+	fetchRemote:fetchRemote, // location given as text (need to parse)
+	fetchNearby: fetchNearby // location isn't given
 };
