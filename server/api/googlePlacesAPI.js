fcb7ee9e46052b0852ea27cf80db19fd82b81888
Aviad Hahami <aviadhahami@gmail.com>
2016-09-02 03:30:59 +0300

    refactored function

--- server/api/googlePlacesAPI.js
+++ server/api/googlePlacesAPI.js
@@ -1,26 +1,37 @@
 'use strict';
 
 const https = require('https');
-
+const q = require('q');
 const key = require('./../secrets').placesKey;
 
 // let location = '32.062102, 34.778758';
 let radius = '500';
 let query = 'italian restaurant in tel aviv';
 
-https.get('https://maps.googleapis.com/maps/api/place/textsearch/json?' //location=' + location
-	+ '&radius=' + radius + '&query=' + query + '&key=' + key + '&libraries=places&output=json', function (response) {
-	var body = '';
-	
-	response.on('data', function (chunk) {
-		body += chunk;
-	});
-	
-	response.on('end', function () {
-		let places = JSON.parse(body);
-		console.log(places.results);
+
+function searchByQuery(query, location, radius) {
+	let deferred = q.defer();
+	https.get('https://maps.googleapis.com/maps/api/place/textsearch/json?' //location=' + location
+		+ '&radius=' + radius + '&query=' + query + '&key=' + key + '&libraries=places&output=json', function (response) {
+		let body = '';
 		
+		response.on('data', function (chunk) {
+			body += chunk;
+		});
+		
+		response.on('end', function () {
+			let places = JSON.parse(body);
+			console.log(places.results);
+			deferred.resolve(places.results);
+		});
+		
+	}).on('error', function (e) {
+		deferred.reject(`Got error: ${e.message}`);
+		console.log(`Got error: ${e.message}`);
 	});
-}).on('error', function (e) {
-	console.log(`Got error: ${e.message}`);
-});
+	return deferred.promise;
+};
+
+module.exports = {
+	searchByQuery: searchByQuery
+};
