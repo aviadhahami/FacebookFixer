fe550aed3f9e191a7d827ef60fedbb8b96f41169
Aviad Hahami <aviadhahami@gmail.com>
2016-09-02 04:39:02 +0300

    removed menu

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 
 
 // Boostrap
-sendApi.callThreadAPI(clientSettingsProvider.generateMenu());
+// sendApi.callThreadAPI(clientSettingsProvider.generateMenu());
 sendApi.callThreadAPI(clientSettingsProvider.generateGreeting());
 
 app.use(express.static(path.join(__dirname,'..','client')));
