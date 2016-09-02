f0d60c0f152c0363c663f34552b8ea71fecaf1ae
Nadav <nadavglickman@gmail.com>
2016-09-02 05:28:47 +0300

    added yelp!

--- server/secrets.js
+++ server/secrets.js
@@ -1,15 +1,24 @@
-/**
- * Created by aviad on 9/1/2016.
- */
 'use strict';
+
+
 
 const twilio = {
 	token: 'ee6a41a35d44f84d7a1e583bafb99cca',
 	SID: 'AC07c6cf721ff99f96502b00bd0327f6b6',
 	self_number: '+9720544932840'
 };
+
+
+const yelpData = {
+	consumer_key: 'gpRqmx7uAednyxzD6vY9pQ',
+	consumer_secret: '6Cwq1bIGGnxhcpCs3q2lVAim1Oo',
+	token: 'Xr4YCT8_3z9ZzaUOpApZYZZT1GuRhAx6',
+	token_secret: 'ONBLsFjONGK5Lr0kzEawAXGNIpI',
+};
+
 const placesKey = 'AIzaSyAIFZ0tjCUja5jsW9UYJ2DxyRnziLD2wtk';
 const token = "EAAPugZCpoV4kBAE8vukyoVinNHZCsgEJTTP6fXHCz3tueJ06dQiDkID5zc4igHUB62ZAM1iwn9q6W2ceguTY1AW551hNwxTqipxbuEi19fZCKg1fG5vbjcy6DFz1ZAAkXAZCeSh5dACOslCLXsZBFVN3ttXDAQGUYJYg2ZATezL64QZDZD"
 module.exports.token = token;
 module.exports.twilio = twilio;
 module.exports.placesKey = placesKey;
+module.exports.yelpData = yelpData;
