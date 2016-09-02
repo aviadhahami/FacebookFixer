dbe80e548669c9981c8b0a1c221d376627d0d050
Aviad Hahami <aviadhahami@gmail.com>
2016-09-02 06:47:11 +0300

    LOL!

--- server/actions/technician.actions.js
+++ server/actions/technician.actions.js
@@ -21,16 +21,24 @@ function fetchRemote(params) {
 
 
 function fetchNearby(params) {
-    let deferred = q.defer();
-    let location = params.location;
-
-    googleAPI.searchByQuery(`${params.type} technician`, parsedLocation, 1000).then(function (res) {
-        deferred.resolve(res);
-    }, function (err) {
-        deferred.reject(err);
-    });
-
-    return deferred.promise;
+	console.log('Fetching nerbby');
+	let deferred = q.defer();
+	let id = params.sessionId;
+	
+	console.log('this is user', activeUsers);
+	let hardCords = {lat:'32.061983',lng: '34.778873'};
+	let hardRadius = 1000;
+	if((!!id && activeUsers.hasOwnProperty(id)) || true){
+		let user = activeUsers[id];
+		googleAPI.searchByQuery(`${params.type} technician`, hardCords, hardRadius).then(function (res) {
+			deferred.resolve(res);
+		}, function (err) {
+			deferred.reject(err);
+		});
+	}else{
+		deferred.reject('No user');
+	}
+	return deferred.promise;
 }
 
 module.exports = {
