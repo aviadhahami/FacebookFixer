ed502a2dee5a5f9f6025a7fb33fc7a6858e1a516
Aviad Hahami <aviadhahami@gmail.com>
2016-09-02 04:43:02 +0300

    deleting menu

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const sendApi = require('./api/sendAPI');
 const clientSettingsProvider = require('./clientSettings/clientSettings');
-const path = require('path');
+const path = require('path')
+const request = require('request');
+const token = require('./secrets').token;
 
 
 let PORT = 5000;
@@ -21,14 +23,23 @@ app.use(bodyParser.json());
 
 // Boostrap
 // sendApi.callThreadAPI(clientSettingsProvider.generateMenu());
+request("https://graph.facebook.com/v2.6/me/thread_settings?access_token=" + token, {
+	method: 'DELETE',
+	json: {
+		"setting_type": "call_to_actions",
+		"thread_state": "existing_thread"
+	}
+}, function (res) {
+	console.log('delte res', res);
+});
 sendApi.callThreadAPI(clientSettingsProvider.generateGreeting());
 
-app.use(express.static(path.join(__dirname,'..','client')));
+app.use(express.static(path.join(__dirname, '..', 'client')));
 
 // Bind routes
 require('./routes')(app);
 
 // Spin up the server
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
 	console.log('running on port', app.get('port'))
 });
