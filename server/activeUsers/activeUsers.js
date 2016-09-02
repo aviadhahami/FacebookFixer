441740f7c5543107682005b79205618d02355481
Aviad Hahami <aviadhahami@gmail.com>
2016-09-02 06:13:01 +0300

    adding activr users

---
+++ server/activeUsers/activeUsers.js
@@ -0,0 +1,9 @@
+/**
+ * Created by aviad on 9/2/2016.
+ */
+
+"use strict";
+
+let users = {};
+
+module.exports = users;
--- server/controllers/userSettings/userSettings.controller.js
+++ server/controllers/userSettings/userSettings.controller.js
@@ -1,10 +1,14 @@
 "use strict";
 
+let activeUsers = require('./../../activeUsers/activeUsers');
 
 function handlePost(req, res) {
 	let id = req.body.user.id;
 	let user = req.body.user;
 	console.log(id,user);
+	if(!!id){
+		activeUsers[id] = user;
+	}
 	res.send(200);
 };
 
